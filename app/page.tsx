56fc128009e6594fbc6b84f9ad9c0536bec2f80a
YxngXi1 <xuetianyang@gmail.com>
2024-08-24 17:47:09 +0800

    done hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Object from "@/components/Object";
+// import Object from "@/components/Object";
 import Image from "next/image";
 import React, { useState, useEffect, useRef } from 'react'
 
@@ -18,37 +18,44 @@ export default function Home() {
         
         console.log(`Hovered over letter: ${spanValue.innerText}`);
         
+        let newAdjective = null;
         if (spanValue.innerText === 'Y') {
-          console.log('Setting adjective to Yachtful');
-          setAdjective('Yachtful');
+          newAdjective = 'Yachtful';
         } else if (spanValue.innerText === 'a') {
-          console.log('Setting adjective to Ambitious');
-          setAdjective('Ambitious');
+          newAdjective = 'Ambitious';
+        } else if (spanValue.innerText === 'n') {
+          newAdjective = 'Nimble';
+        } else if (spanValue.innerText === 'g') {
+          newAdjective = 'Gallant';
+        } else if (spanValue.innerText === 'X') {
+          newAdjective = 'Xenial';
+        } else if (spanValue.innerText === 'u') {
+          newAdjective = 'Unique';
+        } else if (spanValue.innerText === 'e') {
+          newAdjective = 'Enthusiastic';
+        }
+
+        if (newAdjective && newAdjective !== adjective && newAdjective !== adjectiveTyperRef.current?.innerText) {
+          console.log(`Setting isDeleting to true for new adjective: ${newAdjective}`);
+          setIsDeleting(true);
+          setAdjective(newAdjective);
         }
       });
     });
-  }, []);
+  }, [adjective]);
   
   useEffect(() => {
-    if (adjective) {
+    if (adjective && !isDeleting) {
       console.log(`Adjective set to: ${adjective}`);
       let index = 0;
       const interval = setInterval(() => {
         setDisplayedText(prev => {
-          if (isDeleting) {
-            if (prev.length === 0) {
-              setIsDeleting(false);
-              return '';
-            }
-            return prev.slice(0, -1);
-          } else {
-            const nextText = adjective.slice(0, index + 1);
-            index++;
-            if (nextText === adjective) {
-              clearInterval(interval);
-            }
-            return nextText;
+          const nextText = adjective.slice(0, index + 1);
+          index++;
+          if (nextText === adjective) {
+            clearInterval(interval);
           }
+          return nextText;
         });
       }, 100); 
       return () => clearInterval(interval);
@@ -56,51 +63,57 @@ export default function Home() {
   }, [adjective, isDeleting]);
   
   useEffect(() => {
-    console.log(`Displayed text: ${displayedText}`);
-    console.log(`Adjective: ${adjective}`);
-    console.log(`Is deleting: ${isDeleting}`);
-    if (adjectiveTyperRef.current && adjectiveTyperRef.current.innerText !== adjective && !isDeleting) {
-      console.log('Setting isDeleting to true');
-      setIsDeleting(true);
+    if (isDeleting) {
+      const interval = setInterval(() => {
+        setDisplayedText(prev => {
+          if (prev.length === 0) {
+            setIsDeleting(false);
+            return '';
+          }
+          return prev.slice(0, -1);
+        });
+      }, 100);
+      return () => clearInterval(interval);
     }
-  }, [adjective, displayedText]);
+  }, [isDeleting]);
 
   return (
     <>
-      <section className="bg-black min-h-screen flex justify-center">
-        <div 
-          className="absolute"
-          style={{
-            top: '20%',
-            left: '20%',
-            transform: 'translate(-50%, -50%)',
-            zIndex: 0,
-          }}
-        >
-          <Image
-            src='/bggraphic1.png'
-            height={130}
-            width={400}
-            alt="dots"
+      <section className="bg-black flex justify-center items-center min-h-screen">
+        <div className="container flex justify-center min-h-screen">
+          <div 
+            className="absolute"
+            style={{
+              top: '20%',
+              left: '20%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 0,
+            }}
+          >
+            <Image
+              src='/bggraphic1.png'
+              height={130}
+              width={400}
+              alt="dots"
             />
-        </div>
-        <div 
-          className="absolute"
-          style={{
-            top: '70%',
-            left: '60%',
-            transform: 'translate(-50%, -50%)',
-            zIndex: 0,
-          }}
-        >
-          <Image
-            src='/bggraphic2.svg'
-            height={130}
-            width={220}
-            alt="dots"
+          </div>
+          <div 
+            className="absolute"
+            style={{
+              top: '70%',
+              left: '60%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 0,
+            }}
+          >
+            <Image
+              src='/bggraphic2.svg'
+              height={130}
+              width={220}
+              alt="dots"
             />
-        </div>
-        <div 
+          </div>
+          <div 
             className="absolute bg-[#D9D9D9] blur-[100px] w-[100px] h-[100px]"
             style={{
               top: '90%',
@@ -109,27 +122,32 @@ export default function Home() {
               zIndex: 0,
             }}
           ></div>
-        <div className="w-10/12 mx-auto flex justify-center items-start my-auto">
-          
-          <div 
-            className="absolute bg-[#D9D9D9] blur-[100px] w-[541px] h-[55px]"
-            style={{
-              top: '50%',
-              left: '25%',
-              transform: 'translate(-50%, -50%)',
-              zIndex: 0,
-            }}
-          ></div>
-          
-          <div className="flex flex-col gap-y-6 text-left w-7/12">
-            <h1 className="text-white text-8xl font-bold blue-box-shadow name flex gap-x-[2px]"><span>Y</span><span>a</span><span>n</span><span>g</span>&nbsp;<span>X</span><span>u</span><span>e</span></h1>
-            <p className="text-white text-3xl font-light"><u id="adjectiveTyper" ref={adjectiveTyperRef}>{displayedText}</u> Full Stack Developer</p>
-          </div>  
-          <div className="w-5/12">
-            {/* <Object/> */}
+          <div className="w-10/12 mx-auto flex justify-center items-start my-auto">
+            
+            <div 
+              className="absolute bg-[#D9D9D9] blur-[100px] w-[541px] h-[55px]"
+              style={{
+                top: '50%',
+                left: '30%',
+                transform: 'translate(-50%, -50%)',
+                zIndex: 0,
+              }}
+            ></div>
+            
+            <div className="flex flex-col gap-y-6 text-left lg:w-7/12 w-11/12">
+              <h1 className="h-full w-full text-white text-8xl font-bold blue-box-shadow name gap-x-[2px] break-all-words text-wrap">
+                <div className="leading-wide"><span>Y</span><span>a</span><span>n</span><span>g</span>&nbsp;<span>X</span><span>u</span><span>e</span></div>
+              </h1>
+              <p className="text-white text-3xl font-light min-h-[60px]">
+                <u id="adjectiveTyper" ref={adjectiveTyperRef}>{displayedText}</u> Full Stack Developer
+              </p>
+            </div>  
+            <div className="lg:w-5/12">
+              {/* <Object/> */}
+            </div>
           </div>
         </div>
-      </section>
+      </section> 
     </>
   );
 }
--- components/Object.tsx
+++ components/Object.tsx
@@ -1,16 +1,16 @@
-import Spline from '@splinetool/react-spline/next';
-import React from 'react';
+// import Spline from '@splinetool/react-spline/next';
+// import React from 'react';
 
-const Object = () => {
-  return (
-    <main className=''>
-      <div className='transform scale-150'>
-        <Spline
-          scene="https://prod.spline.design/ikHuDF1CSJ24hFUf/scene.splinecode" 
-        />
-      </div>
-    </main>
-  );
-}
+// const Object = () => {
+//   return (
+//     <main className=''>
+//       <div className='transform scale-150'>
+//         <Spline
+//           scene="https://prod.spline.design/ikHuDF1CSJ24hFUf/scene.splinecode" 
+//         />
+//       </div>
+//     </main>
+//   );
+// }
 
-export default Object;
+// export default Object;
