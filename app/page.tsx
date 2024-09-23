680a837a0ba301023edd22135cb7ec94c67cc2a2
Yang <147933670+YxngXi1@users.noreply.github.com>
2024-09-22 21:35:30 -0400

    hide the pages :)

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
   return (
     <>
       <Hero/>
-      <AboutMe/>
+{/*       <AboutMe/>
       <MyWork/>
-      <Contact/>
+      <Contact/> */}
     </>
   );
 }
